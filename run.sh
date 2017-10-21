088e4cf2f17eb2ea2ce4793fc4fe5af3de28c3e9
Asit Dhal <dhal.asitk@gmail.com>
2017-10-21 23:23:11 +0200

    report cleanup

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+mkdir -p build/reports
+
 for file in build/bin/*; do
-    [ -f "$file" ] && [ -x "$file" ] && "$file" --benchmark_out=${file}.csv --benchmark_out_format=csv
+	report_name=$(basename ${file})
+	report_path="build/reports/${report_name}.csv"
+    [ -f "$file" ] && [ -x "$file" ] && "$file" --benchmark_out="${report_path}" --benchmark_out_format=csv
+done
+
+for file in build/reports/*.csv; do
+    sed -i '/WARNING/d' ${file}
+    sed -e '1,2d' ${file}
 done
